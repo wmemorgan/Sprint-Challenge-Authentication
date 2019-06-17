a9322685155039e688d107d37b48d9ac72e9f7e1
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-06-17 11:43:01 +0800

    Fix tablename for case-sensitive database

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs')
 
 exports.seed = async function (knex) {
-  await knex('Users').insert([
+  await knex('users').insert([
     { id: 1, username: 'George', password: bcrypt.hashSync('pass', 12) },
     { id: 2, username: 'Steve', password: bcrypt.hashSync('pass', 12) },
     { id: 3, username: 'Harry', password: bcrypt.hashSync('abc123', 12) }
